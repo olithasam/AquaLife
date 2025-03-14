b94fe2ea6203091ba116e62dd7e9b4490d692636
Olitha Samaraweera <olithasamaraweera2002@gmail.com>
2025-03-14 16:03:42 +0530

    Added html files

---
+++ Scriptsitemap.js
@@ -0,0 +1,47 @@
+
+function function1() {
+    document.getElementById("cla1");
+    addEventListener("click", () => {
+        document.location.href = "home.html"
+    })
+
+}
+
+
+function function2() {
+    document.getElementById("cla2");
+    addEventListener("click", () => {
+        document.location.href = "Shop.html"
+    })
+
+}
+
+function function3() {
+    document.getElementById("cla3");
+    addEventListener("click", () => {
+        document.location.href = "Gallery.html"
+    })
+}
+
+function function4() {
+    document.getElementById("cla4");
+    addEventListener("click", () => {
+        document.location.href = "user.html"
+    })
+
+}
+
+function function5() {
+    document.getElementById("cla5");
+    addEventListener("click", () => {
+        document.location.href = "feedbackform.html"
+    })
+}
+
+function function6() {
+    document.getElementById("cla6");
+    addEventListener("click", () => {
+        document.location.href = "aboutus.html"
+    })
+}
+
